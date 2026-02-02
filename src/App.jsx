43b511db1faac445cbf899b8e7427b85b9b4c801
Artem Kutsan <artemkutsan@gmail.com>
2026-02-02 18:23:29 +0100

    update: Добавлен компонент LIst для второй задачи ДЗ

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,21 @@
 import './App.css';
 import Rating from './components/rating';
+import List from './components/list';
 
 function App() {
   return (
     <>
-      <h1>React - Homework 3</h1>
-      <Rating />
+      <main>
+        <div className="container">
+          <h1>React - Homework 3</h1>
+
+          <h2>Задание 1: Рейтинг</h2>
+          <Rating />
+
+          <h2>Задание 2: Список приглашенных</h2>
+          <List />
+        </div>
+      </main>
     </>
   );
 }
